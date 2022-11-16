5bb334f4cd2557464bd02ea34cab1dde0838f983
SIDDHARTHSSA <WILLLLBEDONE@GMAIL.COM>
2022-11-17 02:19:31 +0530

    first commit for learning

---
+++ src/Class2/String_class2.java
@@ -0,0 +1,10 @@
+package Class2;
+
+public class String_class2 {
+    public static void main(String[] args) {
+    String country = "India" ;
+    System.out.println(country);
+    int noofmatches = 25;
+    System.out.println(noofmatches) ;
+    }
+}
